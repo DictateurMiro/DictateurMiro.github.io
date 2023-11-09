962048cb6fccb26761dc1d0a316b4085f8199f3b
Miro <65116975+DictateurMiro@users.noreply.github.com>
2023-11-09 11:44:11 +0100

    Create index.php

---
+++ CalculIP/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Calcul Adresse IP Locale</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" type="image/x-icon" href="images/globe.png">
+</head>
+<body>
+<div class="main">
+<form action="calcul.php" method="post">
+
+  <center>
+  <label class="texte">Entrez une IPv4 Locale :</label>
+  <table>
+   
+  <tr>
+    <td>
+      <p><input name="1_ip_local_v4" id="1_ip_local_v4" type="text" size="5" maxlength="3"/> .</p>
+    </td>
+     </center>
+    <td>
+      <p><input name="2_ip_local_v4" id="2_ip_local_v4" type="text" size="5" maxlength="3"/> .</p>
+    </td>
+    <td>
+      <p><input name="3_ip_local_v4" id="3_ip_local_v4" type="text" size="5" maxlength="3"/> .</p>
+    </td>
+    <td>
+      <p><input name="4_ip_local_v4" id="4_ip_local_v4" type="text" size="5" maxlength="3"/> /</p>
+    </td>
+    <td>
+      <p><input name="5_ip_local_v4" id="5_ip_local_v4" type="text" size="3" maxlength="3"/></p>
+    </td>
+  </tr>
+  </table>
+
+
+  <button type="submit" class="main_button">Valider</button>
+    </center>
+
+    
+</form>
+</div>
+    
+</body>
+
+  <footer>Developed with the 💙 by <a href="https://github.com/DictateurMiro">Miro</a> & <a href="https://github.com/blobs0">Blob</a></footer>
+
+</html>
+
