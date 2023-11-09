5e3b7b0ea128d2e6ab2a7a2a4fc09001b11a0514
Miro <65116975+DictateurMiro@users.noreply.github.com>
2023-11-09 11:52:54 +0100

    Create 204.php

---
+++ CalculIP/erreur/204.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Erreur 204</title>
+  <link rel="stylesheet" href="../style.css">
+  <link rel="icon" type="image/x-icon" href="../images/interdit.png">
+</head>
+<body>
+<div class="main">
+<center>
+  <p class="title_text">⚠️ Formulaire non complet ⚠️</p>
+  <img src="https://httpducks.com/204.jpg" height="400px" width="400px">
+  <form action="../index.php" method="get">
+    <br>
+    <br>
+    <button type="submit" class="main_button">Recommencer</button>
+  </form>
+</center>
+</div>
+</body>
